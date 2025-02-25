9f93e5211779c5c28eb54110c0882c3732666000
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-02-25 14:54:02 +0000

    [Add]ルーティング修正・部分テンプレート作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers:{
+    sessions: "admin/sessions"
+  }
+  
+  devise_for :users, controllers:{
+    registratons: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: 'homes#top'
