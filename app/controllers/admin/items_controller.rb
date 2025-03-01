bffd7eb3230432371083784ad9421d89d58dffd7
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-03-01 14:53:03 +0000

    [Add]管理者側items/indexページ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,12 +2,14 @@ class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
   
   def new
+    @item = Item.new
   end
 
   def create
   end
 
   def index
+    @items = Item.all
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
     has_one_attached :image
+    
+    
+    def i_image(width, height)
+        unless image.attached?
+            # デフォ画像のパス指定
+            file_path = Rails.root.join('app/assets/images/no-image.jpg')
+            # 画像がない場合、アタッチする記述
+            image.attach(io: File.open(file_path), filename: 'defalult-image.jpg', content_type: 'image/jpeg')
+        end
+        # 画像がアタッチされている時
+        image.variant(resize_to_limit: [width, height]).processed
+    end
+    
 end
